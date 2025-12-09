30b37ec9e8d74ea3a2498ca46bdc1e039625a850
HJR <hjr@HJRdeMacBook-Pro.local>
2025-12-09 17:24:07 +0800

    添加dockerfile

--- scripts/check-users.ts
+++ scripts/check-users.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../src/lib/prisma";
+import { prisma } from "../src/lib/infra/prisma";
 
 async function main() {
   const users = await prisma.user.findMany();
--- scripts/create-user.ts
+++ scripts/create-user.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../src/lib/prisma";
+import { prisma } from "../src/lib/infra/prisma";
 import { hash } from "bcryptjs";
 
 async function main() {
